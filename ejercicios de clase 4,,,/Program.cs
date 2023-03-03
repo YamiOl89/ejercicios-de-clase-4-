2d1246b9c703d4b69e4fc13b7ce41f6c37e0a2eb
50233 <50233@DESKTOP-5OVIGDK>
2023-03-02 20:50:55 -0600

    Agregar archivos de proyecto.

---
+++ ejercicios de clase 4,,,/Program.cs
@@ -0,0 +1,40 @@
+﻿int x = 0;
+//Ejercicio 1
+Console.WriteLine("Ejercicio 1");
+while (x < 500)
+{
+    x++;
+    Console.WriteLine(x);
+}
+Console.WriteLine("Fin!!!");
+
+//Ejercicio 2
+Console.WriteLine("Ejercicio 2");
+x = 49;
+while (x < 100)
+{
+    x++;
+    Console.WriteLine(x);
+}
+Console.WriteLine("Fin!!!");
+
+//Ejercicio 3
+Console.WriteLine("Ejercicio 3");
+x = -51;
+while (x < 0)
+{
+    x++;
+    Console.WriteLine(x);
+}
+Console.WriteLine("Fin!!!");
+
+//Ejercicio 4
+Console.WriteLine("Ejercicio 4");
+x = 2;
+while (x <= 100)
+
+{
+    Console.WriteLine(x);
+    x = x + 2;
+}
+Console.WriteLine("Fin!!!");
